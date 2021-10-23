219f90a72c6e86652490574a1b5a611962ee7c05
shlok299 <shlok2999@gmail.com>
2021-10-23 17:48:34 +0530

    Testing Remains

---
+++ dsa_ass3/2021201046_Q2c.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<queue>
+#include<climits>
+using namespace std;
+
+bool is_valid(vector<vector<int>> &mat,int i,int j);
+
+int main()
+{
+    int r,c;
+    cin>>r;
+    cin>>c;
+    vector<pair<int,int>> rider;
+    pair<int,int> a;
+    vector<vector<int>> mat(r,vector<int>(c,-1));
+
+    
+
+    for(int i=0;i<r;i++)
+    {
+        string s;
+        cin>>s;
+        for(int j=0;j<c;j++)
+        {
+            char ch=s[j];
+            if(ch=='#')
+            {
+                mat[i][j]=INT_MAX;
+            }
+            else if(ch=='R')
+            {
+                rider.push_back({i,j});
+            }
+            else if(ch=='A')
+            {
+                a={i,j};
+            }
+        }
+    }
+    mat[1][0]=0;
+
+    queue<pair<int,int>> q;
+    q.push({1,0});
+    while(!q.empty())
+    {
+        pair<int,int> point=q.front();
+        q.pop();
+        int i=point.first;
+        int j=point.second;
+        if(is_valid(mat,i-1,j))
+        {
+            mat[i-1][j]=mat[i][j]+1;
+            q.push({i-1,j});
+        }
+        if(is_valid(mat,i+1,j))
+        {
+            mat[i+1][j]=mat[i][j]+1;
+            q.push({i+1,j});
+        }
+        if(is_valid(mat,i,j-1))
+        {
+            mat[i][j-1]=mat[i][j]+1;
+            q.push({i,j-1});
+        }
+        if(is_valid(mat,i,j+1))
+        {
+            mat[i][j+1]=mat[i][j]+1;
+            q.push({i,j+1});
+        }
+    }
+
+    bool check=true;
+    int ans=mat[a.first][a.second];
+    for(int i=0;i<rider.size();i++)
+    {
+        int x=rider[i].first;
+        int y=rider[i].second;
+        if(mat[x][y]<ans)
+        {
+            check=false;
+            break;
+        }
+    }
+    if(check)
+    {
+        cout<<"YES\n";
+        cout<<ans<<endl;
+    }
+    else
+    {
+        cout<<"NO";
+    }
+    // for(int i=0;i<r;i++)
+    // {
+    //     for(int j=0;j<c;j++)
+    //         cout<<mat[i][j]<<" ";
+    //     cout<<endl;
+    // }
+}
+
+bool is_valid(vector<vector<int>> &mat,int i,int j)
+{
+    int row=mat.size();
+    int col=mat[0].size();
+
+    if(i<0 || i>=row || j<0 || j>=col)
+        return false;
+    if(mat[i][j]>=0)
+        return false;
+    else 
+        return true;
+}
