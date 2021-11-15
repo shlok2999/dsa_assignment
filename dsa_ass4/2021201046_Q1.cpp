d8f886ac64d328c3718a9eac053f87744e206635
shlok299 <shlok2999@gmail.com>
2021-11-15 19:18:35 +0530

    Completed and tested Q1

--- dsa_ass4/2021201046_Q1.cpp
+++ dsa_ass4/2021201046_Q1.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<algorithm>
 #include<climits>
+#include<map>
 using namespace std;
 
 class suffix
@@ -44,50 +45,61 @@ int main()
 
 vector<int> suffix_array(string s)
 {
-    int size=s.length();
-    vector<suffix> ans(size);
-    for(int i=0;i<size;i++)
+    // int size=s.length();
+    // vector<suffix> ans(size);
+    // for(int i=0;i<size;i++)
+    // {
+    //     ans[i].index=i;
+    //     ans[i].rank.first=(int)(s[i]);
+    //     if(i+1<size)
+    //         ans[i].rank.second=(int)(s[i+1]);
+    //     else
+    //         ans[i].rank.second=-1;
+
+    // }
+    // sort(ans.begin(),ans.end());
+    // vector<int> arr(size);
+    // for(int k=2;k<2*size;k*=2)
+    // {
+    //     int nr=0;
+    //     int prev=ans[0].rank.first;
+    //     ans[0].rank.first=nr;
+    //     arr[ans[0].index]=0;
+    //     for(int i=1;i<size;i++)
+    //     {
+    //         if(ans[i].rank.first!=prev || ans[i].rank.second!=ans[i-1].rank.second )
+    //             nr++;
+    //         prev=ans[i].rank.first;
+    //         ans[i].rank.first=nr;
+    //         arr[ans[i].index]=i;
+    //     }
+
+    //     for(int i=0;i<size;i++)
+    //     {
+    //         int new_substr_index=ans[i].index + k;
+    //         if(new_substr_index>=size)
+    //             ans[i].rank.second=-1;
+    //         else
+    //             ans[i].rank.second=ans[arr[new_substr_index]].rank.first;
+    //     }
+
+    //     sort(ans.begin(),ans.end());
+    // }
+
+    // vector<int> sa;
+    // for(suffix ss:ans)
+    //     sa.push_back(ss.index);
+    map<string,int> suffixes;
+    vector<int>sa;
+    string suffix="";
+    for(int i=s.length()-1;i>=0;i--)
     {
-        ans[i].index=i;
-        ans[i].rank.first=(int)(s[i]);
-        if(i+1<size)
-            ans[i].rank.second=(int)(s[i+1]);
-        else
-            ans[i].rank.second=-1;
-
+        suffix=s[i]+suffix;
+        suffixes[suffix]=i;
     }
-    sort(ans.begin(),ans.end());
-    vector<int> arr(size);
-    for(int k=2;k<2*size;k*=2)
-    {
-        int nr=0;
-        int prev=ans[0].rank.first;
-        ans[0].rank.first=nr;
-        arr[ans[0].index]=0;
-        for(int i=1;i<size;i++)
-        {
-            if(ans[i].rank.first!=prev || ans[i].rank.second!=ans[i-1].rank.second )
-                nr++;
-            prev=ans[i].rank.first;
-            ans[i].rank.first=nr;
-            arr[ans[i].index]=i;
-        }
-
-        for(int i=0;i<size;i++)
-        {
-            int new_substr_index=ans[i].index + k;
-            if(new_substr_index>=size)
-                ans[i].rank.second=-1;
-            else
-                ans[i].rank.second=ans[arr[new_substr_index]].rank.first;
-        }
-
-        sort(ans.begin(),ans.end());
-    }
-
-    vector<int> sa;
-    for(suffix ss:ans)
-        sa.push_back(ss.index);
+    sa.clear();
+    for(auto suffix:suffixes)
+        sa.push_back(suffix.second);
     return sa;
 }
 
